b3de19f7c988a921bc8d987363f66f8e959e4490
ywu256 <ywu256@my.centennialcollege.ca>
2025-01-28 15:02:07 -0500

    Implement store student data

--- app/src/main/java/com/example/mapd721_assignment1/MainActivity.kt
+++ app/src/main/java/com/example/mapd721_assignment1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,19 +16,25 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mapd721_assignment1.ui.theme.MAPD721_assignment1Theme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -36,11 +43,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MAPD721_assignment1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     StudentApp()
                 }
             }
@@ -51,42 +57,59 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun StudentApp() {
-    var id by remember { mutableStateOf("538") }
-    var username by remember { mutableStateOf("") }
-    var courseName by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val studentNumValue = remember {
+        mutableStateOf(TextFieldValue())
+    }
+    val studentNameValue = remember {
+        mutableStateOf(TextFieldValue())
+    }
+    val courseNameValue = remember {
+        mutableStateOf(TextFieldValue())
+    }
+    val store = StudentStore(context)
+
+    var loadedStudentNum by remember { mutableStateOf("") }
+    var loadedStudentName by remember { mutableStateOf("") }
+    var loadedCourseName by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
+            .clickable { keyboardController?.hide() }
             .fillMaxSize()
             .padding(20.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         TextField(
-            value = id,
-            onValueChange = { id = it },
+            value = studentNumValue.value,
+            onValueChange = { studentNumValue.value = it },
             label = { Text("ID") },
-            readOnly = true
         )
         TextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Username") }
+            value = studentNameValue.value,
+            onValueChange = { studentNameValue.value = it },
+            label = { Text("Student Name") }
         )
         TextField(
-            value = courseName,
-            onValueChange = { courseName = it },
+            value = courseNameValue.value,
+            onValueChange = { courseNameValue.value = it },
             label = { Text("Course Name") }
         )
         Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
             Button(
-                onClick = {  },
+                onClick = {},
                 modifier = Modifier.size(width = 130.dp, height = 45.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow)
             ) {
                 Text("Load", color = MaterialTheme.colorScheme.primary)
             }
             Button(
-                onClick = {  },
+                onClick = {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        store.saveStudentData(studentNumValue.value.text, studentNameValue.value.text, courseNameValue.value.text)
+                    }
+                },
                 modifier = Modifier.size(width = 130.dp, height = 45.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Color.Green)
             ) {
@@ -95,12 +118,15 @@ fun StudentApp() {
 
         }
         Button(
-            onClick = {  },
+            onClick = { },
             modifier = Modifier.size(width = 276.dp, height = 45.dp),
             colors = ButtonDefaults.buttonColors(containerColor = Color.Cyan)
         ) {
             Text("Reset", color = MaterialTheme.colorScheme.primary)
         }
+        Text(text = "Loaded ID: $loadedStudentNum")
+        Text(text = "Loaded Student Name: $loadedStudentName")
+        Text(text = "Loaded Course Name: $loadedCourseName")
         Spacer(modifier = Modifier.height(16.dp))
         Text("Student Name: Yu-Ling Wu")
         Text("Student ID: 301434538")
--- app/src/main/java/com/example/mapd721_assignment1/StudentStore.kt
+++ app/src/main/java/com/example/mapd721_assignment1/StudentStore.kt
@@ -1,4 +1,29 @@
 package com.example.mapd721_assignment1
 
-class StudentStore {
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+
+class StudentStore(private val context: Context) {
+    companion object {
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("student_data")
+        private val STUDENT_NUM_KEY = stringPreferencesKey("student_num")
+        private val STUDENT_NAME_KEY = stringPreferencesKey("student_name")
+        private val COURSE_NAME_KEY = stringPreferencesKey("course_name")
+    }
+
+    // Store student data
+    suspend fun saveStudentData(studentNum: String, studentName: String, courseName: String) {
+        context.dataStore.edit { preferences ->
+            preferences[STUDENT_NUM_KEY] = studentNum
+            preferences[STUDENT_NAME_KEY] = studentName
+            preferences[COURSE_NAME_KEY] = courseName
+        }
+    }
 }
